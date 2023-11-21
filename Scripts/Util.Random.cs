e5b01aff40a28026a3f6b656eb43f603c972b1f2
Noah Ratcliff <noah@aesthetic.games>
2023-11-21 12:12:52 -0500

    Add Util.Random.AlphanumericString

--- Scripts/Util.Random.cs
+++ Scripts/Util.Random.cs
@@ -7,9 +7,29 @@ namespace AeLa.Utilities
 	{
 		public static class Random
 		{
+			private static readonly System.Random cachedRandom = new();
+
 			public static Vector2Int Range(Vector2Int min, Vector2Int max)
 			{
 				return new(URandom.Range(min.x, max.x + 1), URandom.Range(min.y, max.y + 1));
+			}
+
+			/// <summary>
+			/// Returns a random alphanumeric string of the provided length--similar to a hash
+			/// </summary>
+			/// <param name="length">The length of the string</param>
+			public static string AlphanumericString(uint length)
+			{
+				const string characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+				var ret = string.Empty;
+
+				for (int i = 0; i < length; i++)
+				{
+					ret += characters[cachedRandom.Next(characters.Length)];
+				}
+
+				return ret;
 			}
 		}
 	}
