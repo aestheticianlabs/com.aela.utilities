4a0cddb94a8a9d54789ea702ddd8681516199724
Noah Ratcliff <noah@aesthetic.games>
2024-05-23 11:28:20 -0400

    Add IUseLogLevel

---
+++ Scripts/Debugging/IUseLogLevel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace AeLa.Utilities.Debugging
+{
+	public interface IUseLogLevel
+	{
+		public LogLevel LogLevel { get; set; }
+	}
+
+	public static class IUseLogLevelExtensions
+	{
+		public static void Log(this IUseLogLevel source, LogType logType, object message, Object context = null)
+		{
+			DebugFiltered.Log(logType, source.LogLevel, message, context);
+		}
+
+		public static void Log(this IUseLogLevel source, object message, Object context = null)
+		{
+			DebugFiltered.Log(source.LogLevel, message, context);
+		}
+
+
+		public static void LogWarning(this IUseLogLevel source, object message, Object context = null)
+		{
+			DebugFiltered.LogWarning(source.LogLevel, message, context);
+		}
+
+		public static void LogError(this IUseLogLevel source, object message, Object context = null)
+		{
+			DebugFiltered.LogError(source.LogLevel, message, context);
+		}
+
+		public static void LogException(this IUseLogLevel source, Exception exception, Object context = null)
+		{
+			DebugFiltered.LogException(source.LogLevel, exception, context);
+		}
+	}
+}
