77ae5eceb326e746e194170b54ea50bb84194964
Noah Ratcliff <noah@aesthetic.games>
2024-08-28 08:11:45 -0400

    Update weighted random list property drawer

--- Editor/WeightedRandomListPropertyDrawer.cs
+++ Editor/WeightedRandomListPropertyDrawer.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 namespace AeLa.Utilities.Editor
 {
 	[CustomPropertyDrawer(typeof(WeightedRandomList<>))]
-	public class WeightedRandomListPropertyDrawer : PropertyDrawer 
+	public class WeightedRandomListPropertyDrawer : PropertyDrawer
 	{
+		private const string ItemsProp = nameof(WeightedRandomList<object>.Items);
+
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			EditorGUI.PropertyField(position, property.FindPropertyRelative("items"), label);
+			EditorGUI.PropertyField(position, property.FindPropertyRelative(ItemsProp), label);
 		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
-			return EditorGUI.GetPropertyHeight(property.FindPropertyRelative("items"), label);
+			return EditorGUI.GetPropertyHeight(property.FindPropertyRelative(ItemsProp), label);
 		}
 	}
 
 	[CustomPropertyDrawer(typeof(WeightedRandomList<>.WeightedRandomItem))]
 	public class WeightedRandomListElementPropertyDrawer : ForceExpandedPropertyDrawer
 	{
-		public WeightedRandomListElementPropertyDrawer() : base("Item", "Weight") { }
+		private const string ItemProp = nameof(WeightedRandomList<object>.WeightedRandomItem.Item);
+		private const string WeightProp = nameof(WeightedRandomList<object>.WeightedRandomItem.Weight);
+
+		public WeightedRandomListElementPropertyDrawer() : base(ItemProp, WeightProp) { }
 	}
 }
