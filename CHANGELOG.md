c05ecfc8825215b53e4d37d8d2f06f7cbb0697a7
Noah Magnolia <noah@aesthetic.games>
2024-11-19 16:43:56 -0500

    IList queue/stack extension methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Extension methods
 	- `GetUniqueRandom`
+  - Stack/queue API for `IList<T>`: `Peek`, `Push`, `Pop`, `Enqueue`, `Dequeue`
 
 ### Fixed
 
--- Scripts/Extensions.cs
+++ Scripts/Extensions.cs
@@ -292,6 +292,29 @@ namespace AeLa.Utilities
 			return nearest;
 		}
 
+		public static T Peek<T>(this IList<T> list) => list[0];
+
+		public static T Pop<T>(this IList<T> list)
+		{
+			var item = list[0];
+			list.RemoveAt(0);
+			return item;
+		}
+
+		public static void Push<T>(this IList<T> list, T item) => list.Insert(0, item);
+
+		public static T Dequeue<T>(this IList<T> list)
+		{
+			var item = list[0];
+			list.RemoveAt(0);
+			return item;
+		}
+
+		public static void Enqueue<T>(this IList<T> list, T item)
+		{
+			list.Add(item);
+		}
+
 		/// <summary>
 		/// Gets the connected anchor for this joint in world space, whether or not there is a connected body.
 		/// </summary>
