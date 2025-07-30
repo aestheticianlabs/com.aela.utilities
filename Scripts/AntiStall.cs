7a3200213f79e767fb462947fc322116da5b1a46
Noah Magnolia <noah@aesthetic.games>
2025-07-30 18:41:30 -0400

    Add AntiStall helper

---
+++ Scripts/AntiStall.cs
@@ -0,0 +1,32 @@
+﻿namespace AeLa.Utilities
+{
+	/// <summary>
+	/// Prevents loops from accidentally hard-locking the app
+	/// </summary>
+	public struct AntiStall
+	{
+		private const int defaultMax = 10000;
+
+		private int remaining;
+
+		private AntiStall(int maxIterations)
+		{
+			remaining = maxIterations;
+		}
+
+		public bool Check()
+		{
+			if (remaining-- <= 0)
+			{
+				throw new($"{nameof(AntiStall)} triggered");
+			}
+
+			return true;
+		}
+
+		public static AntiStall Create(int maxIterations = defaultMax)
+		{
+			return new(maxIterations);
+		}
+	}
+}
