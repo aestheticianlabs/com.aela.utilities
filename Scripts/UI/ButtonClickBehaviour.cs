bb184b81a68cc56850e47285c3fdbf9e2b14f988
Noah Magnolia <noah@aesthetic.games>
2026-04-17 10:43:13 -0400

    Add ButtonClickBehaviour

---
+++ Scripts/UI/ButtonClickBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AeLa.Utilities.UI
+{
+	[RequireComponent(typeof(Button))]
+	public abstract class ButtonClickBehaviour : MonoBehaviour
+	{
+		protected Button Button;
+
+		protected virtual void Awake()
+		{
+			Button = GetComponent<Button>();
+			Button.onClick.AddListener(Button_OnClick);
+		}
+
+		protected virtual void OnDestroy()
+		{
+			Button.onClick.RemoveListener(Button_OnClick);
+		}
+
+		protected abstract void Button_OnClick();
+	}
+}
